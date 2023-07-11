be61474877d2855a7d62e78e1eb121f23b62c6f9
tommywaltion <tommywaltion@gmail.com>
2023-07-11 11:54:30 +0700

    Delete html directory

--- html/template_fasilitas.php
+++
@@ -1,4 +0,0 @@
-<div class="template-facility">
-  <div class="template-facility-image-wrapper"><img src=<?php echo $datas["image"]; ?> alt="asset/template_image.png"></div>
-  <b class="template-facility-nama"><?php echo $datas["nama"] ?></b>
-</div>
